0f04f3ae9071cbdc68921cfbffb806d19dafbe73
ohaddahan <ohaddahan@gmail.com>
2025-04-06 15:50:53 +0300

    add mint to fe

--- zkSOL-webapp/solita/pda/merkle_pda.ts
+++ zkSOL-webapp/solita/pda/merkle_pda.ts
@@ -18,27 +18,32 @@ import {
 import BN from "bn.js";
 import { CryptoHelper } from "../crypto-helpers";
 
-export function getMerkleAddress(depth: number): [PublicKey, number] {
+export function getMerkleAddress(
+  depth: number,
+  mint: PublicKey
+): [PublicKey, number] {
   return PublicKey.findProgramAddressSync(
     [
       Buffer.from(anchor.utils.bytes.utf8.encode(MerkleSeed)),
+      mint.toBuffer(),
       new BN(depth).toBuffer("le", 8),
     ],
-    PROGRAM_ID,
+    PROGRAM_ID
   );
 }
 
 export async function getMerkleAccount(
   connection: Connection,
   depth: number,
+  mint: PublicKey
 ): Promise<MerkleState> {
-  const [merkle] = getMerkleAddress(depth);
+  const [merkle] = getMerkleAddress(depth, mint);
   return await MerkleState.fromAccountAddress(connection, merkle);
 }
 
 export function getMerkleTokenAddress(
   depth: number,
-  mint: PublicKey,
+  mint: PublicKey
 ): [PublicKey, number] {
   return PublicKey.findProgramAddressSync(
     [
@@ -46,21 +51,23 @@ export function getMerkleTokenAddress(
       new BN(depth).toBuffer("le", 8),
       mint.toBuffer(),
     ],
-    PROGRAM_ID,
+    PROGRAM_ID
   );
 }
 
 export function getMerklePendingProofAddress(
   depth: number,
   index: number,
+  mint: PublicKey
 ): [PublicKey, number] {
   return PublicKey.findProgramAddressSync(
     [
       Buffer.from(anchor.utils.bytes.utf8.encode(MerklePendingProofSeed)),
+      mint.toBuffer(),
       new BN(depth).toBuffer("le", 8),
       new BN(index).toBuffer("le", 8),
     ],
-    PROGRAM_ID,
+    PROGRAM_ID
   );
 }
 
@@ -68,32 +75,38 @@ export async function getMerklePendingProofAccount(
   connection: Connection,
   depth: number,
   index: number,
+  mint: PublicKey
 ): Promise<MerklePendingProofState> {
-  const [leaf] = getMerklePendingProofAddress(depth, index);
+  const [leaf] = getMerklePendingProofAddress(depth, index, mint);
   return await MerklePendingProofState.fromAccountAddress(connection, leaf);
 }
 
-export function getMerkleZerosAddress(depth: number): [PublicKey, number] {
+export function getMerkleZerosAddress(
+  depth: number,
+  mint: PublicKey
+): [PublicKey, number] {
   return PublicKey.findProgramAddressSync(
     [
       Buffer.from(anchor.utils.bytes.utf8.encode(MerkleZerosSeed)),
+      mint.toBuffer(),
       new BN(depth).toBuffer("le", 8),
     ],
-    PROGRAM_ID,
+    PROGRAM_ID
   );
 }
 
 export async function getMerkleZerosAccount(
   connection: Connection,
   depth: number,
+  mint: PublicKey
 ): Promise<MerkleZeros> {
-  const [merkle] = getMerkleZerosAddress(depth);
+  const [merkle] = getMerkleZerosAddress(depth, mint);
   return await MerkleZeros.fromAccountAddress(connection, merkle);
 }
 
 export function getMerkleNodeAddress(
   depth: number,
-  index: number,
+  index: number
 ): [PublicKey, number] {
   return PublicKey.findProgramAddressSync(
     [
@@ -101,14 +114,14 @@ export function getMerkleNodeAddress(
       new BN(depth).toBuffer("le", 8),
       new BN(index).toBuffer("le", 8),
     ],
-    PROGRAM_ID,
+    PROGRAM_ID
   );
 }
 
 export async function getMerkleNodeAccount(
   connection: Connection,
   depth: number,
-  index: number,
+  index: number
 ): Promise<MerkleNodeState> {
   const [merkle] = getMerkleNodeAddress(depth, index);
   return await MerkleNodeState.fromAccountAddress(connection, merkle);
@@ -116,7 +129,7 @@ export async function getMerkleNodeAccount(
 
 export function getNullifierHashAddress(
   depth: number,
-  nullifier_hash: number[],
+  nullifier_hash: number[]
 ): [PublicKey, number] {
   return PublicKey.findProgramAddressSync(
     [
@@ -124,14 +137,14 @@ export function getNullifierHashAddress(
       new BN(depth).toBuffer("le", 8),
       Buffer.from(nullifier_hash),
     ],
-    PROGRAM_ID,
+    PROGRAM_ID
   );
 }
 
 export async function getNullifierHashAccount(
   connection: Connection,
   depth: number,
-  nullifier_hash: number[],
+  nullifier_hash: number[]
 ): Promise<NullifierHash> {
   const [hash] = getNullifierHashAddress(depth, nullifier_hash);
   return await NullifierHash.fromAccountAddress(connection, hash);
@@ -139,7 +152,7 @@ export async function getNullifierHashAccount(
 
 export async function isgNullifierHashUsed(
   connection: Connection,
-  nullifier_hash: number[],
+  nullifier_hash: number[]
 ): Promise<boolean> {
   const accounts = await NullifierHash.gpaBuilder()
     .addFilter("nullifierHash", nullifier_hash)
